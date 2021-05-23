d861039d4c09f5b604839bd26526bfb75b9e5acb
dolphius21 <johnrudolphlee@gmail.com>
2021-05-23 13:36:23 +0800

    Setup Header component and MUI theme

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import { ThemeProvider } from '@material-ui/core/styles';
+import theme from './ui/Theme';
+import Header from './ui/Header';
+
+function App() {
+  return (
+    <ThemeProvider theme={theme}>
+      <Header />
+      Hello!
+    </ThemeProvider>
+  );
+}
+
+export default App;
