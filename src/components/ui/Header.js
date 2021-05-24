c76bdd046ddbd6018c380e2554d7243ede41714f
dolphius21 <johnrudolphlee@gmail.com>
2021-05-24 19:00:35 +0800

    Done styling and routing the appbar links

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -1,7 +1,15 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+// materialUI imports
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import Button from '@material-ui/core/Button';
 import useScrollTrigger from '@material-ui/core/useScrollTrigger';
+import { makeStyles } from '@material-ui/styles';
+// internal imports
+import logo from '../../assets/logo.svg';
 
 function ElevationScroll(props) {
   const { children } = props;
@@ -15,13 +23,129 @@ function ElevationScroll(props) {
   });
 }
 
+const useStyles = makeStyles((theme) => ({
+  toolbarMargin: {
+    ...theme.mixins.toolbar,
+    marginBottom: '3em'
+  },
+  logo: {
+    height: '7em',
+    display: 'block'
+  },
+  logoContainer: {
+    padding: 0
+  },
+  tabContainer: {
+    marginLeft: 'auto',
+    '&:hover': {
+      backgroundColor: 'transparent'
+    }
+  },
+  tab: {
+    ...theme.typography.tab,
+    minWidth: 10,
+    marginLeft: '25px'
+  },
+  button: {
+    ...theme.typography.estimate,
+    borderRadius: '50px',
+    margin: '2rem',
+    height: '45px'
+  }
+}));
+
 const Header = (props) => {
+  const styles = useStyles();
+  const [value, setValue] = useState(0);
+
+  const handleChange = (e, value) => {
+    setValue(value);
+  };
+
+  useEffect(() => {
+    if (window.location.pathname === '/' && value !== 0) {
+      setValue(0);
+    } else if (window.location.pathname === '/services' && value !== 1) {
+      setValue(1);
+    } else if (window.location.pathname === '/revolution' && value !== 2) {
+      setValue(2);
+    } else if (window.location.pathname === '/about' && value !== 3) {
+      setValue(3);
+    } else if (window.location.pathname === '/contact' && value !== 4) {
+      setValue(4);
+    } else if (window.location.pathname === '/estiamates' && value !== 5) {
+      setValue(5);
+    }
+  }, [value]);
+
   return (
-    <ElevationScroll>
-      <AppBar>
-        <Toolbar>Arc Developement</Toolbar>
-      </AppBar>
-    </ElevationScroll>
+    <>
+      <ElevationScroll>
+        <AppBar>
+          <Toolbar disableGutters>
+            <Link
+              to="/"
+              className={styles.logoContainer}
+              onClick={handleChange}
+            >
+              <img
+                className={styles.logo}
+                src={logo}
+                alt="Arc Development Logo"
+              />
+            </Link>
+            <Tabs
+              value={value}
+              onChange={handleChange}
+              className={styles.tabContainer}
+              indicatorColor="primary"
+            >
+              <Tab
+                className={styles.tab}
+                component={Link}
+                to="/"
+                label="Home"
+              />
+              <Tab
+                className={styles.tab}
+                component={Link}
+                to="/services"
+                label="Services"
+              />
+              <Tab
+                className={styles.tab}
+                component={Link}
+                to="/revolution"
+                label="The Revolution"
+              />
+              <Tab
+                className={styles.tab}
+                component={Link}
+                to="/about"
+                label="About Us"
+              />
+              <Tab
+                className={styles.tab}
+                component={Link}
+                to="/contact"
+                label="Contact Us"
+              />
+            </Tabs>
+            <Button
+              component={Link}
+              to="/estimates"
+              onClick={handleChange}
+              variant="contained"
+              color="secondary"
+              className={styles.button}
+            >
+              Free Estimate
+            </Button>
+          </Toolbar>
+        </AppBar>
+      </ElevationScroll>
+      <div className={styles.toolbarMargin} />
+    </>
   );
 };
 
--- src/components/ui/Theme.js
+++ src/components/ui/Theme.js
@@ -1,3 +1,41 @@
 import { createMuiTheme } from '@material-ui/core/styles';
 
-export default createMuiTheme({});
+const arcBlue = '#0B72B9';
+const arcOrange = '#FFBA60';
+
+export default createMuiTheme({
+  palette: {
+    common: {
+      blue: `${arcBlue}`,
+      orange: `${arcOrange}`
+    },
+    primary: {
+      main: `${arcBlue}`,
+      light: '#59a0ec',
+      dark: '#004789'
+    },
+    secondary: {
+      main: `${arcOrange}`,
+      light: '#ffec90',
+      dark: '#c88a31'
+    }
+  },
+  typography: {
+    tab: {
+      fontFamily: 'Raleway',
+      textTransform: 'none',
+      fontWeight: 700,
+      fontSize: '1rem'
+    },
+    estimate: {
+      fontFamily: 'Pacifico',
+      textTransform: 'none',
+      fontSize: '1rem',
+      color: 'white',
+      '&:hover': {
+        backgroundColor: 'black',
+        color: 'white'
+      }
+    }
+  }
+});
